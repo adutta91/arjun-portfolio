6a25c290a788d21268625a76053c633fb495546c
Arjun <arjundutta91@gmail.com>
2018-05-15 22:07:51 -0700

    add footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 
 export default class Footer extends Component {
+    openTab(url) {
+        if(url) window.open(url, '_blank');
+    }
+    
     render() {
         return (
             <div id='contact-info' className='footer'>
-                <div className="email">Email: <a href="mailto:arjundutta91@gmail.com">arjundutta91@gmail.com</a></div>
                 <div className="icons">
-                    <i className="fab fa-github" />
-                    <i className="fab fa-linkedin-in" />
-                    <i className="far fa-file-alt" />
+                    <i className="fab fa-2x fa-github" onClick={this.openTab.bind(this, 'https://github.com/adutta91')}/>
+                    <i className="fab fa-2x fa-linkedin-in" onClick={this.openTab.bind(this, 'https://linkedin.com/in/arjundutta91/')}/>
+                    <i className="far fa-2x fa-file-alt" onClick={this.openTab.bind(this, '')}/>
                 </div>
+                <a href="mailto:arjundutta91@gmail.com">arjundutta91@gmail.com</a>
             </div>
         );
     }
