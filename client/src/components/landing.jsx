30be601b08d5b0bbe5c8c0f79698663792785156
Arjun <arjundutta91@gmail.com>
2018-05-19 17:08:12 -0700

    landing placeholder

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Landing extends Component {    
+    state = {
+        fade : false
+    }
+    
+    componentDidMount() {
+        this.timeout = setTimeout(() => {
+            this.setState({ fade : true });
+        }, 1000);
+    }
+    
+    componentWillUnmount() {
+        clearTimeout(this.timeout);
+    }
+    
     render() {
         return (
             <div className='container-content landing'>
-                
+                <h1 className={`${this.state.fade ? 'in' : ''}`}> Find me where I love to be. </h1>
             </div>
         );
     }
