4a327b628a3c1d9cc95455ad516b5f9d922d442d
Arjun <arjundutta91@gmail.com>
2018-08-06 22:42:34 -0700

    add scaffolding for other skill endpoints

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,29 @@ const Skill = require('../mongoose/skill');
 
 router.get('/test', (req, res) => {
     res.json({ success : true });
-    // Skill.find({}).exec((err, skills) => {
-    //     res.json(skills);
-    // });
+});
+
+// fetches all skills
+router.get('/skills', (req, res) => {
+    Skill.find({}).exec((err, skills) => {
+        res.json(skills);
+    });
+});
+
+// creates a skill
+router.post('/skills', (req, res) => {
+    res.json({}); 
+});
+
+// fetches a single skill
+router.get('/skills/:id', (req, res) => {
+    console.log('req.params *****---->>>', req.params);
+    res.json({}); 
+});
+
+// deletes a single skill
+router.delete('/skills/:id', (req, res) => {
+    res.json({});
 });
 
 module.exports = router;
