5325efad7f16c6522e410e4834662ed62af7e40a
Arjun <arjundutta91@gmail.com>
2018-08-13 22:55:16 -0700

    use prod db

--- index.js
+++ index.js
@@ -7,24 +7,23 @@ const app = express();
 const api = require('./routes/api');
 const bodyParser = require('body-parser');
 
-//configuring bodyparser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-
 const mongoose = require('mongoose');
 // TODO: figure out environment variables
-mongoose.connect('mongodb://localhost:27017/local');
-// mongoose.connect('mongodb://portfolio:mongobongo42@ds113942.mlab.com:13942/arjun_portfolio', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/local');
+mongoose.connect('mongodb://portfolio:mongobongo42@ds113942.mlab.com:13942/arjun_portfolio', { useNewUrlParser: true });
 
 app.db = mongoose.connection;
 app.db.on('error', () => { console.log('---FAILED to connect to mongoose') });
 app.db.once('open', () => {
   console.log('+++Connected to mongoose');
 });
+
+//configuring bodyparser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
   
 app.use('/api', api);
 
