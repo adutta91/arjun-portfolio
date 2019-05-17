7d75c4842946eaaf19ca989b2b0145fcc0027af0
Arjun <arjundutta91@gmail.com>
2019-05-16 23:14:50 -0700

    copy changes

--- client/src/app/projects.js
+++ client/src/app/projects.js
@@ -9,21 +9,21 @@ const projects = [
     {
         id: 3,
         name : 'React Viewable',
-        description : `React wrapper component to only show content once it's in view. Designed to be performative so as not to slow down navigation of the site.`,
+        description : `React wrapper component to only show content once it's in the viewport. Designed to be performative so as not to slow down navigation of the site.`,
         technologies : ['react', 'javascript', 'npm'],
         image : 'viewable.gif'
     },
     {
         id: 2,
         name : 'WYSIWYG Blog',
-        description : `Internal blogging system used by over 200 people daily. Features include a dynamic WYSIWYG editor with markdown keyboard shortcuts, an infinitely scrolling blog index, and flexible article scheduling functionality.`,
+        description : `Internal blogging system used by hundreds of people daily. Features include a dynamic WYSIWYG editor with markdown keyboard shortcuts, an infinitely scrolling blog index, and flexible article scheduling functionality.`,
         technologies : ['node', 'express', 'react', 'redux', 'mysql', 'javascript'],
         image : 'blog.gif'
     },
     {
         id: 1,
         name : 'React Playground Projects',
-        description : 'Mini-projects playing around with JavaScript and React for various utilities, including JavaScript Semantic Parsing, a "Typeable" React wrapper, and a "Revolver" navigation menu with an arbitrary depth.',
+        description : 'Projects playing around with JavaScript and React for various utilities, including JavaScript Semantic Parsing, a "Typeable" React wrapper, and a "Revolver" navigation menu with an arbitrary depth.',
         technologies : ['react', 'javascript'],
         image : 'typing.gif'
     },
